4f11cc4ecdad70227b2d6b131837b9b2bfbf99f5
Jane-Sim <ssiox3@gmail.com>
2021-09-06 21:28:36 +0900

    생성했던 내비게이션바 루트 컴포넌트인 index.vue에 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="container">
-    <Hero />
-    <LargeCardDisplay
-      v-for="cardInfo in largeCardInfo"
-      :key="cardInfo.id"
-      :cardSection="cardInfo"
-    />
-    <SmallCardDisplay
-      v-for="cardInfo in smallCardSections"
-      :key="cardInfo.id"
-      :cardsSection="cardInfo"
-    />>
+  <div>
+    <Nav />
+    <div class="container">
+      <Hero />
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />>
+    </div>
   </div>
 </template>
 
